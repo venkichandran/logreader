2730f11df014ccd8cffed940b439a7c968a9e550
Venki Chandran <venkatesan.chandran@sage.com>
2021-11-28 12:31:09 -0800

    API add target files  commit

--- src/main/java/com/logreader/restservice/LogReaderController.java
+++ src/main/java/com/logreader/restservice/LogReaderController.java
@@ -1,15 +1,33 @@
 package com.logreader.restservice;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
+import com.logreader.exception.LogReaderException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LogReaderController {
 
-    private final AtomicLong counter = new AtomicLong();
-    
-    @GetMapping("/logreader")
-    public LogOutput readLogs(@RequestParam(value = "fileName", defaultValue = "World") String fileName) {
-        return new LogOutput(counter.incrementAndGet(), String.format(template, fileName));
-    }
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/logreader")
+	public LogOutput readLogs(@RequestParam(value = "fileName", defaultValue = "World") String fileName) {
+		return new LogOutput(counter.incrementAndGet(), String.format(template, fileName));
+	}
+
+	@PostMapping("/logstreamer")
+	public List<LogOutput> searchLogs(@RequestBody LogInput input) throws LogReaderException, FileNotFoundException, IOException {
+//		List<LogOutput> output = new ArrayList<LogOutput>();
+//		output.add(new LogOutput(counter.incrementAndGet(), String.format(template, input.getFileName())));
+//		return  output;
+		return new LogReader().processRequest(input);
+	}
 }
